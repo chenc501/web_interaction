4665daf78b69632717ba7322625c3a139fc9b344
chenc501 <54692233+chenc501@users.noreply.github.com>
2019-09-21 14:39:03 -0400

    Add files via upload

---
+++ exercise4/js/scripts.js
@@ -0,0 +1,56 @@
+var date = new Date();
+
+function showTimeAMPM(){
+    var h = date.getHours(); 
+    var m = date.getMinutes(); 
+    var s = date.getSeconds(); 
+    var session = "AM";
+    
+    if(h == 12){
+        session = "PM"
+      }
+
+    if(h == 0){
+        h = 12; // nighttime
+    }
+    
+    if(h > 12){ // applying to noon this is only for h == 13
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("clock").innerText = time;
+//     document.body.style.backgroundColor = document.body.style.backgroundColor == "skyBlue" ? "pink" : "skyBlue";
+	document.getElementById("clock").style.color = document.getElementById("clock").style.color == "pink" ? "yellow" : "pink";
+   
+    setTimeout(showTimeAMPM, 1000);
+
+	var y= date.getFullYear();
+	var mt = date.getMonth() + 1;
+	var d = date.getDate();
+
+	var datetime = y + "/" + mt + "/" + d;
+	document.getElementById("d1").innerText = datetime;
+    
+};
+
+showTimeAMPM();
+
+
+setInterval(function(){
+
+	var randomNumber = Math.random() * 300 + "px";
+	$('#clock').css('margin-left', randomNumber);
+
+	var randomNumberDeg = "rotate(" +Math.random() * 100 + "deg)";
+	$('#clock').css('transform', randomNumberDeg);
+
+},1000)
+
+
+
