25711aa363c9d1d068b615b8c20f8023e9125cc7
chenc501 <54692233+chenc501@users.noreply.github.com>
2019-12-04 16:11:09 -0500

    Add files via upload

---
+++ performance1/js/scripts.js
@@ -0,0 +1,38 @@
+// var word= document.getElementById("word");
+// 	square = document.getElementById("square");
+//     clickMe = document.getElementById('clickMe'); 
+
+
+// clickMe.onclick = doBk;
+
+// function doBk () {
+
+//   square.style.backgroundColor = "black";
+//   document.getElementById('word').style.color = 'white';
+
+// }
+
+// function resetBk() {
+//     square.style.backgroundColor = "black";
+//     document.getElementById('word').style.color = 'black';
+//  }
+
+$('#circle').click(function(){
+
+	$('#circle').css('transform', 'rotate(0deg)')
+})
+
+
+$('#resultButton').click(function(){
+    $('#result').fadeIn();
+
+})
+
+$('#resultButton1').click(function(){
+    $('#result1').fadeIn();
+
+})
+
+
+
+
